af9e17359750bb244c8c8efb882681f1a731cd59
jayesh parmar <parmarjayu2510@gmail.com>
2025-03-19 20:06:09 +0530

    js change

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
 const btn=document.querySelectorAll("button")
 const resultele=document.getElementById("result")
-
 const userscoreele=document.getElementById("user-score")
 const compscoreele=document.getElementById("comp-score")
-let userscore=0
-let compscore=0
+let userscore = 0
+let compscore = 0
 btn.forEach((e)=>{
     e.addEventListener("click",()=>{
     // console.log("you clicked on",btn.id);
@@ -29,15 +28,18 @@ function playround( userselection, compselection){
     }else if(
         (userselection==="rock"&& compselection ==="scissor")||
         (userselection==="paper"&& compselection ==="rock")||  (userselection==="scissor"&& compselection ==="paper")
-    ){
-        return `you won ! ${userselection} beats ${compselection}`
+    )
+    {
         userscore++
-        userscoreele.textContent=userscore
-    }
-    else{
-        return `you lose! ${compselection}beats ${compselection}`
-        compscore++
+        // console.log(userscore);
+        userscoreele.textContent = userscore
+        return `you won ! ${userselection} beats ${compselection}`
+    }else{
+        compscore++;
+        // console.log(compscore) 
         compscoreele.textContent=compscore
+        return `you lose! ${userselection} beats ${compselection}`
+         
     }
     
 } 
